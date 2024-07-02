d35d4070170c667482d0166b2ced9912b33f5d35
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-07-02 00:53:31 +0000

    Software Sales Example

--- .ccls-cache/@home@runner@Chapter-4-Program-Examples-Replit/main.cpp
+++ .ccls-cache/@home@runner@Chapter-4-Program-Examples-Replit/main.cpp
@@ -1,8 +1,53 @@
-// Chapter 4 Program Examples - there are 4 programs, each on separate
-// branches.
+// Chapter 4 Example Project.cpp : This file contains the 'main' function for the Software Sales program. 
 
 #include <iostream>
+#include <iomanip>
 
-int main() {
-  std::cout << "Chapter 4 Program Examples\n";
+using namespace std;
+
+int main()
+{
+    const double DISCOUNT_RATE1 = .2;
+    const double DISCOUNT_RATE2 = .3;
+    const double DISCOUNT_RATE3 = .4;
+    const double DISCOUNT_RATE4 = .5;
+
+    const int NO_DISCOUNT = 9;
+    const int DISCOUNT1 = 19;
+    const int DISCOUNT2 = 49;
+    const int DISCOUNT3 = 99;
+
+    const double packagePrice = 99;
+
+    int unitsSold;
+    double discount = 0, purchaseCost;
+
+    cout << "Enter the number of packages purchased: ";
+    cin >> unitsSold;
+    if (unitsSold <= 0)
+    {
+        cout << " You must purchase at least one package." << endl;
+        return(1);
+    }
+
+
+
+    if (unitsSold <= NO_DISCOUNT)
+        discount = 0;
+    else if (unitsSold <= DISCOUNT1)
+        discount = DISCOUNT_RATE1;
+    else if (unitsSold <= DISCOUNT2)
+        discount = DISCOUNT_RATE2;
+    else if (unitsSold <= DISCOUNT3)
+        discount = DISCOUNT_RATE3;
+    else
+        discount = DISCOUNT_RATE4;
+
+    purchaseCost = (packagePrice - packagePrice * discount) * unitsSold;
+
+    cout << fixed << setprecision(2) << showpoint;
+    cout << "You bought " << unitsSold << " for a total of $" << purchaseCost << endl;
+
+    return 0;
+
 }
